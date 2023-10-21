ce85798a96099c6168e6bb70814616c60861fb5f
Spencer <scbelton7@gmail.com>
2023-10-20 22:42:57 -0600

    Prep for new branch adding Chat GPT.

--- MorningFormula/MF/Model/Managers/SpotifyManager.swift
+++ MorningFormula/MF/Model/Managers/SpotifyManager.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 class SpotifyManager: ObservableObject {
-    static let instance = SpotifyManager()
+    
+    static let instance = SpotifyManager(spotifyService: SpotifyAPIService())
+    
+    private var spotifyService: SpotifyService
     
     private var accessCode = ""
-    private var accessTokenTimer: Timer?
+    var accessTokenTimer: Timer?
     
     @Published var pauseSearch = false {
         didSet {
@@ -39,13 +42,15 @@ class SpotifyManager: ObservableObject {
     }
     @ObservedObject var errorManager = ErrorManager.instance
     
+    init(spotifyService: SpotifyService) {
+        self.spotifyService = spotifyService
+    }
+    
     func getAccessCodeTapped() {
-        self.getAccessToken { token, error in
+        self.spotifyService.getAccessToken { token, error in
             if let token = token {
                 self.accessCode = token
-                Timer.scheduledTimer(withTimeInterval: 1200, repeats: false, block: { timer in
-                    self.accessTokenTimer = timer
-                })
+                self.startTimer(duration: 2)
             }
         }
     }
@@ -53,7 +58,7 @@ class SpotifyManager: ObservableObject {
     func searchSongs(_ query: String) {
         let keepSearching = !pauseSearch
         if keepSearching {
-            self.getSongFromSearch(query) { root, error in
+            spotifyService.getSongFromSearch(accessCode: accessCode, query) { root, error in
                 if let error = error {
                     self.errorManager.setError(error.localizedDescription)
                 }
@@ -67,10 +72,45 @@ class SpotifyManager: ObservableObject {
             }
         }
     }
+
     
-    func getSongFromSearch(_ query: String, withCompletion completion: @escaping(SpotifyRoot?, Error?) -> Void) {
+    private func handleSearchPauseTimer() {
+        var timer: Timer?
+        var secondsRemaining = 1
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if secondsRemaining > 0 {
+                secondsRemaining -= 1
+            } else {
+                timer?.invalidate() // Stop the timer when countdown reaches 0
+                self.pauseSearch = false
+            }
+        }
+    }
+    
+    func startTimer(duration: TimeInterval) {
+        Timer.scheduledTimer(withTimeInterval: duration, repeats: false, block: { timer in
+            self.accessTokenTimer = timer
+            print(timer)
+        })
+    }
+}
+
+protocol SpotifyService {
+    
+    /// Token
+    func getAccessToken(withCompletion completion: @escaping(String?, Error?) -> Void)
         
-        if let request = urlRequestFromQuery(query) {
+    /// Search
+    func getSongFromSearch(accessCode: String, _ query: String, withCompletion completion: @escaping(SpotifyRoot?, Error?) -> Void)
+    func urlRequestFromQuery(accessCode: String, _ query: String) -> URLRequest?
+
+}
+
+class SpotifyAPIService: SpotifyService {
+    
+    func getSongFromSearch(accessCode: String, _ query: String, withCompletion completion: @escaping(SpotifyRoot?, Error?) -> Void) {
+        
+        if let request = urlRequestFromQuery(accessCode: accessCode, query) {
             let session = URLSession.shared
             let task = session.dataTask(with: request) { data, response, error in
                 if let error = error {
@@ -89,13 +129,12 @@ class SpotifyManager: ObservableObject {
         }
     }
     
-    func urlRequestFromQuery(_ query: String) -> URLRequest? {
+    func urlRequestFromQuery(accessCode: String, _ query: String) -> URLRequest? {
         guard var baseURL = URLComponents(string: "https://api.spotify.com/v1/search") else {
-//            error(NSError(domain: "SpotifyManager", code: 3, userInfo: ["message" : "BaseURL is nil"]))
             return nil
         }
         
-        let authKey = "Bearer \(self.accessCode)"
+        let authKey = "Bearer \(accessCode)"
         
         
         let headers = [
@@ -170,224 +209,4 @@ class SpotifyManager: ObservableObject {
         }
     }
     
-//    func spotifySearch(query: String, withCompletion completion: @escaping(Track?, Error?) -> Void) {
-//
-//    }
-    
-    
-    private func handleSearchPauseTimer() {
-        var timer: Timer?
-        var secondsRemaining = 1
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            if secondsRemaining > 0 {
-                secondsRemaining -= 1
-            } else {
-                timer?.invalidate() // Stop the timer when countdown reaches 0
-                self.pauseSearch = false
-            }
-        }
-    }
 }
-
-
-////
-////  SpotifyManager.swift
-////  MorningFormula
-////
-////  Created by Spencer Belton on 10/6/23.
-////
-//
-//import SwiftUI
-//
-//class SpotifyManager: ObservableObject {
-//    static let instance = SpotifyManager()
-//    
-//    private var accessCode = ""
-//    private var accessTokenTimer: Timer?
-//    
-//    @Published var pauseSearch = false {
-//        didSet {
-//            if pauseSearch == true {
-//                self.handleSearchPauseTimer()
-//            }
-//        }
-//    }
-//    
-//    @Published var searchInput = ""
-////    {
-////        didSet {
-////            if searchInput.count > 2 {
-////                self.searchSongs(searchInput)
-////            }
-////        }
-////    }
-//    
-//    @Published var songTitlesForInputGroup: [String] = []
-//    @Published var songs: [SpotifyItem] = [] {
-//        didSet {
-//            let songTitles = songs.map( { $0.stringValue })
-//            songTitlesForInputGroup = songTitles
-//        }
-//    }
-//    @ObservedObject var errorManager = ErrorManager.instance
-//    
-//    func getAccessCodeTapped() {
-//        self.getAccessToken { token, error in
-//            if let token = token {
-//                self.accessCode = token
-//                Timer.scheduledTimer(withTimeInterval: 1200, repeats: false, block: { timer in
-//                    self.accessTokenTimer = timer
-//                    print(timer)
-//                })
-//            }
-//        }
-//    }
-//    
-////    func searchSongs(_ query: String) {
-////        let keepSearching = !pauseSearch
-////        if keepSearching {
-////            self.getSongFromSearch(query) { root, error in
-////                if let error = error {
-////                    self.errorManager.setError(error.localizedDescription)
-////                }
-////                if let root = root {
-////                    let newSongs = root.tracks.items
-////                    DispatchQueue.main.async {
-////                        self.songs = newSongs
-////                        self.pauseSearch = true
-////                    }
-////                }
-////            }
-////        }
-////    }
-//
-//    
-//    private func handleSearchPauseTimer() {
-//        var timer: Timer?
-//        var secondsRemaining = 1
-//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-//            if secondsRemaining > 0 {
-//                secondsRemaining -= 1
-//            } else {
-//                timer?.invalidate() // Stop the timer when countdown reaches 0
-//                self.pauseSearch = false
-//            }
-//        }
-//    }
-//}
-//
-//protocol SpotifyService {
-//    
-//    /// Token
-//    func getAccessToken(withCompletion completion: @escaping(String?, Error?) -> Void)
-//        
-//    /// Search
-//    func getSongFromSearch(_ query: String, withCompletion completion: @escaping(SpotifyRoot?, Error?) -> Void)
-//    func urlRequestFromQuery(_ query: String) -> URLRequest?
-//
-//}
-//
-//class SpotifyAPIService: SpotifyService {
-//    
-//    func getSongFromSearch(_ query: String, withCompletion completion: @escaping(SpotifyRoot?, Error?) -> Void) {
-//        
-//        if let request = urlRequestFromQuery(query) {
-//            let session = URLSession.shared
-//            let task = session.dataTask(with: request) { data, response, error in
-//                if let error = error {
-//                    print(error.localizedDescription)
-//                }
-//                if let data = data {
-//                    do {
-//                        let root = try JSONDecoder().decode(SpotifyRoot.self, from: data)
-//                        completion(root, nil)
-//                    } catch {
-//                        print(error.localizedDescription)
-//                    }
-//                }
-//            }
-//            task.resume()
-//        }
-//    }
-//    
-//    func urlRequestFromQuery(accessCode: String, _ query: String) -> URLRequest? {
-//        guard var baseURL = URLComponents(string: "https://api.spotify.com/v1/search") else {
-//            return nil
-//        }
-//        
-//        let authKey = "Bearer \(accessCode)"
-//        
-//        
-//        let headers = [
-//            "Authorization" : authKey
-//        ]
-//        
-//        baseURL.queryItems = [
-//            URLQueryItem(name: "q", value: query),
-//            URLQueryItem(name: "type", value: "track"),
-//            URLQueryItem(name: "limit", value: "3")
-//        ]
-//        
-//        baseURL.percentEncodedQuery = baseURL.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
-//        if let url = baseURL.url {
-//            let request = NSMutableURLRequest(url: url)
-//            request.httpMethod = "GET"
-//            request.allHTTPHeaderFields = headers
-//            return request as URLRequest
-//        }
-//            return nil
-//    }
-//    
-//    
-//    func getAccessToken(withCompletion completion: @escaping(String?, Error?) -> Void) {
-//        
-//        let headers = [
-//            "Content-Type" : "application/x-www-form-urlencoded",
-//            "Authorization" : "Basic NDk1MjFmYzk3MTUyNGFiZGFmZWY0YTlhMGM4MTA5YmY6MDRlYTU1ZWQyMzE2NGJjMGFkNzJkNjc0ZDg1NDk3MzQ"
-//            ]
-//        let parameters = [
-//            "grant_type" : "client_credentials"
-//        ]
-//        
-//        
-//        if let url = NSURL(string: "https://accounts.spotify.com/api/token") {
-//            let request = NSMutableURLRequest(url: url as URL)
-//            request.httpMethod = "POST"
-//            request.allHTTPHeaderFields = headers
-//            request.httpBody = parameters
-//                .map { key, value in "\(key)=\(value)" }
-//                .joined(separator: "&")
-//                .data(using: .utf8)
-//            
-//            let session = URLSession.shared
-//            
-//            let dataTask = session.dataTask(with: request as URLRequest) {
-//                data, response, error in
-//                if let error = error {
-//                    print(error.localizedDescription)
-//                }
-//                if let data = data {
-//
-//                    do {
-//                        if let json = try JSONSerialization.jsonObject(with: data) as? [String : Any] {
-//                            print(json)
-//                            if let accessToken = json["access_token"] as? String {
-//                                completion(accessToken, nil)
-//                            } else {
-//                                completion(nil, NSError(domain: "SpotifyManager", code: 1, userInfo: ["message" : "Access Token not found in response"]))
-//                            }
-//                        } else {
-//                            completion(nil, NSError(domain: "SpotifyManager", code: 2, userInfo: ["message" : "Invalid JSON response."]))
-//                        }
-//                    } catch {
-//                        print(error.localizedDescription)
-//                        completion(nil, error)
-//                    }
-//                }
-//            }
-//            dataTask.resume()
-//
-//        }
-//    }
-//    
-//}
